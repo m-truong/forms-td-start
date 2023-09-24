ccfaef7d518de6071eb38c7f1a726d0b568d1c5f
Michael Truong <truong.michael.2013@gmail.com>
2023-09-24 16:09:40 -0400

    Add notes on the FormsModule for Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// This is the FormsModule that angular sues
+// this import is required for the "Template-Driven" Forms approach...
+// it's the automatic inference of the form's object shape
 import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/core'
 
 
 import { AppComponent } from './app.component';
@@ -12,6 +16,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
