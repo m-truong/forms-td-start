2f5a8771b2bf54d30089418d6c25e03a34c386ea
Michael Truong <truong.michael.2013@gmail.com>
2023-09-29 15:21:55 -0400

    Update ngSubmit method
    Update onSubmit method to use ViewChild decorator and have direct reference to the component's template.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,12 +7,22 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // Reviewing: @ViewChild decorator gives direct access to the NgForm or even the ElementRef inside the component's template
+  @ViewChild('form') signupForm: NgForm; // store within variable
+  // By storing the form template within the current Angular component ---> I'm essentially getting acces to the NgForm model even BEFORE submitting the form!
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
   // this angular helper f(x) gets triggered everytime on Submit
-  onSubmit(form: NgForm) {
-    console.log('=> Logging the NgForm newly created object', form);
+  // onSubmit(form: NgForm) {
+  //   console.log('=> Logging the NgForm newly created object', form);
+  // }
+  onSubmit() {
+    console.log(
+      '=> Logging the variable signupForm, which stores the NgForm',
+      this.signupForm,
+    );
   }
 }
